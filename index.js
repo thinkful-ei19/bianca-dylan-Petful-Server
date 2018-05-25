bd778b84d8506385223d644c38e99d04bd217e9d
biancavee15 <vasquezbmarie@gmail.com>
2018-05-25 11:23:45 -0400

    added get endpoints for dogs and cats

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
-
+let dogArray = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}, {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}];
+let catArray = [{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}];
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,7 +46,18 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+app.get('/api/cat',(req, res)=> {
+  res.json(catArray[0]);
+});
+app.delete('/api/cat', (req, res)=> {
 
+});
+app.get('/api/dog',(req,res)=> {
+  res.json(dogArray[0]);
+});
+app.delete('/api/dog', (req,res)=> {
+
+});
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
